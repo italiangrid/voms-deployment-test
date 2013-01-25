3a5abefe545975d1f5d44adb35d2b25d2bedc9f5
root <root@jen01.cnaf.infn.it>
2013-01-25 13:35:59 +0100

    A few changes on the deployment script for emi-voms-mysql on SL5

--- emi-voms-mysql-upgrade-deployment-sl5.sh
+++ emi-voms-mysql-upgrade-deployment-sl5.sh
@@ -14,60 +14,60 @@ execute_cmd() {
 }
  
 # install the EMI-2 emi-release package
-#execute_cmd 'wget http://emisoft.web.cern.ch/emisoft/dist/EMI/2/sl5/x86_64/base/emi-release-2.0.0-1.sl5.noarch.rpm'
-#execute_cmd 'rpm -i emi-release-2.0.0-1.sl5.noarch.rpm'
+execute_cmd 'wget http://emisoft.web.cern.ch/emisoft/dist/EMI/2/sl5/x86_64/base/emi-release-2.0.0-1.sl5.noarch.rpm'
+execute_cmd 'rpm -i emi-release-2.0.0-1.sl5.noarch.rpm'
  
 # clean yum
-#execute_cmd 'yum clean all'
+execute_cmd 'yum clean all'
  
 # install emi-voms-mysql
-#execute_cmd 'yum install -y --nogpgcheck emi-voms-mysql'
+execute_cmd 'yum install -y --nogpgcheck emi-voms-mysql'
 
 # install xml-commons-apis
-#execute_cmd 'yum install -y --nogpgcheck xml-commons-apis'
+execute_cmd 'yum install -y --nogpgcheck xml-commons-apis'
  
 # start mysql and configure root access
-#execute_cmd '/etc/init.d/mysqld start'
-#execute_cmd '/usr/bin/mysqladmin -u root password pwd'
+execute_cmd '/etc/init.d/mysqld start'
+execute_cmd '/usr/bin/mysqladmin -u root password pwd'
  
 # configure voms using yaim
-#execute_cmd "cat > site-info.def << EOF
-#MYSQL_PASSWORD="pwd"
-#SITE_NAME="voms-certification.cnaf.infn.it"
-#VOS="testvo"
-#VOMS_HOST=`hostname -f`
-#VOMS_DB_HOST='localhost'
-#VO_TESTVO_VOMS_PORT=15000
-#VO_TESTVO_VOMS_DB_USER=voms
-#VO_TESTVO_VOMS_DB_PASS=pwd
-#VO_TESTVO_VOMS_DB_NAME=voms_emi2to3
-#VOMS_ADMIN_SMTP_HOST=postino.cnaf.infn.it
-#VOMS_ADMIN_MAIL=andrea.ceccanti@cnaf.infn.it
-#EOF"
-#execute_cmd '/opt/glite/yaim/bin/yaim -c -s site-info.def -n VOMS'
+execute_cmd "cat > site-info.def << EOF
+MYSQL_PASSWORD="pwd"
+SITE_NAME="voms-certification.cnaf.infn.it"
+VOS="testvo"
+VOMS_HOST=`hostname -f`
+VOMS_DB_HOST='localhost'
+VO_TESTVO_VOMS_PORT=15000
+VO_TESTVO_VOMS_DB_USER=voms
+VO_TESTVO_VOMS_DB_PASS=pwd
+VO_TESTVO_VOMS_DB_NAME=voms_emi2to3
+VOMS_ADMIN_SMTP_HOST=postino.cnaf.infn.it
+VOMS_ADMIN_MAIL=andrea.ceccanti@cnaf.infn.it
+EOF"
+execute_cmd '/opt/glite/yaim/bin/yaim -c -s site-info.def -n VOMS'
 
 # wait a while
-#execute_cmd 'sleep 30'
+execute_cmd 'sleep 30'
  
 # check voms-admin can list groups
-#execute_cmd 'voms-admin --vo testvo list-users'
+execute_cmd 'voms-admin --vo testvo list-users'
  
 # check a user can be added to the vo
-#execute_cmd 'cp /usr/share/igi-test-ca/test0.cert.pem .'
-#execute_cmd 'chmod 400 test0.cert.pem'
-#execute_cmd 'voms-admin --vo testvo create-user test0.cert.pem'
+execute_cmd 'cp /usr/share/igi-test-ca/test0.cert.pem .'
+execute_cmd 'chmod 400 test0.cert.pem'
+execute_cmd 'voms-admin --vo testvo create-user test0.cert.pem'
  
 # install voms clients
-#execute_cmd 'yum install -y voms-clients'
+execute_cmd 'yum install -y voms-clients'
  
 # prepare vomsdir and vomses
-#execute_cmd 'mkdir .glite'
-#execute_cmd 'cp /etc/voms-admin/testvo/vomses .glite'
+execute_cmd 'mkdir .glite'
+execute_cmd 'cp /etc/voms-admin/testvo/vomses .glite'
  
 # check voms-proxy-init
-#execute_cmd 'cp /usr/share/igi-test-ca/test0.key.pem .'
-#execute_cmd 'chmod 400 test0.key.pem'
-#execute_cmd 'echo pass | voms-proxy-init --cert test0.cert.pem --key test0.key.pem --voms testvo --pwstdin'
+execute_cmd 'cp /usr/share/igi-test-ca/test0.key.pem .'
+execute_cmd 'chmod 400 test0.key.pem'
+execute_cmd 'echo pass | voms-proxy-init --cert test0.cert.pem --key test0.key.pem --voms testvo --pwstdin'
 
 # Stop the services
 execute_cmd "service voms stop"
@@ -80,6 +80,7 @@ execute_cmd "yum -y remove emi-release"
 # Download EMI 3 repos & VOMS repos
 execute_cmd "wget -P /etc/yum.repos.d http://eticssoft.web.cern.ch/eticssoft/mock/emi-3-rc-sl5.repo"
 execute_cmd "wget -P /etc/yum.repos.d http://etics-repository.cern.ch/repository/pm/volatile/repomd/id/e0e65f4c-8a74-4763-98b2-20d4cf317714/sl5_x86_64_gcc412EPEL/etics-volatile-build-by-id-protect.repo"
+execute_cmd 'echo priority=1 >> /etc/yum.repos.d/etics-volatile-build-by-id-protect.repo'
 
 # clean yum
 execute_cmd "yum clean all"
@@ -94,3 +95,12 @@ execute_cmd "yum -y install emi-release"
 execute_cmd "yum -y install emi-voms-mysql"
 execute_cmd "yum -y update"
 
+# configure voms
+execute_cmd 'voms-configure install --dbtype mysql --vo testvo --dbauser root --dbapwd pwd --dbusername voms --dbpassword pwd --core-port 15001 --admin-port 15002 --smtp-host postino.cnaf.infn.it --mail-from valerio.venturi@cnaf.infn.it'
+
+# start service
+execute_cmd "service voms stop"
+execute_cmd "service voms-admin stop"
+
+# voms-proxy-init
+execute_cmd 'echo pass | voms-proxy-init --cert test0.cert.pem --key test0.key.pem --voms testvo --pwstdin'
