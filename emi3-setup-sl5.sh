7a89845d0bdbc4874485b33d29816113246c527d
Andrea Ceccanti <andrea.ceccanti@cnaf.infn.it>
2013-01-25 19:42:20 +0100

    Update emi3-setup-sl5.sh

--- emi3-setup-sl5.sh
+++ emi3-setup-sl5.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+export DEFAULT_EMI2_RELEASE_PACKAGE=http://emisoft.web.cern.ch/emisoft/dist/EMI/2/sl5/x86_64/base/emi-release-2.0.0-1.sl5.noarch.rpm
 export DEFAULT_EMI_REPO=http://eticssoft.web.cern.ch/eticssoft/mock/emi-3-rc-sl5.repo
 export DEFAULT_VOMS_REPO=http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_voms_SL5/lastSuccessfulBuild/artifact/voms.repo
