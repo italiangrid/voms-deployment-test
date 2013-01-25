e6b8ab01e75bf5d538635bbe9682cf94e8ae8d16
Andrea Ceccanti <andrea.ceccanti@cnaf.infn.it>
2013-01-25 19:13:57 +0100

    Added setup scripts.

---
+++ emi3-setup-sl5.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export DEFAULT_EMI_REPO=http://eticssoft.web.cern.ch/eticssoft/mock/emi-3-rc-sl5.repo
+export DEFAULT_VOMS_REPO=http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_voms_SL5/lastSuccessfulBuild/artifact/voms.repo
---
+++ emi3-setup-sl6.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export DEFAULT_EMI_REPO=http://eticssoft.web.cern.ch/eticssoft/mock/emi-3-rc-sl6.repo
+export DEFAULT_VOMS_REPO=http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_voms_SL6/lastSuccessfulBuild/artifact/voms.repo
