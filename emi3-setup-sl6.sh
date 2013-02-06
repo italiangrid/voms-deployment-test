8cead3bee50e9392c57d716de086a205e582982e
Andrea Ceccanti <andrea.ceccanti@cnaf.infn.it>
2013-02-06 15:04:20 +0100

    Changed default repo for sl6

--- emi3-setup-sl6.sh
+++ emi3-setup-sl6.sh
@@ -2,7 +2,7 @@
 
 export DEFAULT_EMI2_RELEASE_PACKAGE=http://emisoft.web.cern.ch/emisoft/dist/EMI/2/sl6/x86_64/base/emi-release-2.0.0-1.sl6.noarch.rpm
 export DEFAULT_EMI_REPO=http://eticssoft.web.cern.ch/eticssoft/mock/emi-3-rc-sl6.repo
-export DEFAULT_VOMS_REPO=http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_voms_SL6/lastSuccessfulBuild/artifact/voms.repo
+export DEFAULT_VOMS_REPO=http://etics-repository.cern.ch/repository/pm/volatile/repomd/id/18fa2a60-fe44-43f1-8db2-a1989d34f474/sl6_x86_64_gcc446EPEL/etics-volatile-build-by-id-protect.repo
 
 # Do MySQL deployment tests by default
 export VOMS_METAPACKAGE=emi-voms-mysql
