78ed580f18804f5c873330227356e65aac2c4be5
Andrea Ceccanti <andrea.ceccanti@cnaf.infn.it>
2013-10-07 15:55:32 +0200

    Skip gpg checks on upgrade deployment test.

--- voms-api-java-upgrade-deployment.sh
+++ voms-api-java-upgrade-deployment.sh
@@ -3,7 +3,7 @@ source common.sh
  
 execute "mkdir emi-release-package"
 execute "wget -P emi-release-package $emi_release_package"
-execute "yum -y localinstall emi-release-package/*.rpm"
+execute "yum -y --nogpgcheck localinstall emi-release-package/*.rpm"
 execute "yum clean all"
 execute "yum -y install voms-api-java"
 
@@ -16,7 +16,7 @@ install_voms_repo
 # clean yum
 execute "yum clean all"
 
-execute "yum -y install emi-release"
+execute "yum -y --nogpgcheck install emi-release"
 execute "yum -y update"
 
 execute "yum -y install voms-api-java3"
