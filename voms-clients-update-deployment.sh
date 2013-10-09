fa4963beab3aec4f17a4ff36333c2904b50ddd8d
Andrea Ceccanti <andrea.ceccanti@cnaf.infn.it>
2013-10-09 17:08:31 +0200

    Leverage common.sh in client scripts.

--- voms-clients-update-deployment.sh
+++ voms-clients-update-deployment.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
 set -e
-voms_repo=$DEFAULT_VOMS_REPO
-
-voms_repo_filename="/etc/yum.repos.d/voms_update.repo"
-hostname=$(hostname -f)
-
-execute() {
-  echo "[root@`hostname` ~]# $1"
-  eval "$1" || ( echo "Deployment failed"; exit 1 )
-}
+source common.sh
 
 echo "VOMS clients update deployment test"
 
-if [ ! -z "$voms_repo" ]; then
-    echo "Update repo URL: $voms_repo"
-fi
-
-if [ ! -z "$voms_repo" ]; then
-    execute "wget -q $voms_repo -O $voms_repo_filename"
-    execute "echo >> $voms_repo_filename; echo 'priority=1' >> $voms_repo_filename"
-fi
+install_voms_repo
 
 # Clean yum database
 execute "yum clean all"
--- voms-clients-upgrade-deployment.sh
+++ voms-clients-upgrade-deployment.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
  
 set -e
-emi_release_package=$DEFAULT_EMI2_RELEASE_PACKAGE
+source common.sh
 
-emi_repo=$DEFAULT_EMI_REPO
-voms_repo=$DEFAULT_VOMS_REPO
-
-emi_repo_filename="/etc/yum.repos.d/test_emi.repo"
-voms_repo_filename="/etc/yum.repos.d/test_voms.repo"
-
-hostname=$(hostname -f)
 clients_package=voms-clients3
 
 [ $# -eq 1 ] && clients_package=$1
- 
-[ -z "$emi_release_package" ] && ( echo "Please set the DEFAULT_EMI2_RELEASE_PACKAGE env variable!"; exit 1 )
-[ -z "$emi_repo" ]  && ( echo "Please set the DEFAULT_EMI_REPO env variable!"; exit 1 )
-[ -z "$clients_package" ]  && ( echo "Usage: $0 <clients_package>"; exit 1 )
-
-execute() {
-  echo "[root@`hostname` ~]# $1"
-  eval "$1" || ( echo "Deployment failed"; exit 1 )
-}
  
 configure_vomsdir(){
   execute "mkdir -p /etc/grid-security/vomsdir"
   execute "cp /etc/grid-security/hostcert.pem /etc/grid-security/vomsdir"
 }
+
+
+## Deployment starts HERE
+
+install_test_ca_repo
 
 execute "mkdir emi-release-package"
 execute "wget -P emi-release-package $emi_release_package"
@@ -34,25 +23,17 @@ execute "yum -y --nogpgcheck localinstall emi-release-package/*.rpm"
 execute "yum clean all"
 execute "yum -y install voms-clients"
 
-# Setup certificate for voms-proxy-init test
-execute "mkdir -p .globus"
-execute "cp /usr/share/igi-test-ca/test0.cert.pem .globus/usercert.pem"
-execute "cp /usr/share/igi-test-ca/test0.key.pem .globus/userkey.pem"
-execute "chmod 600 .globus/usercert.pem"
-execute "chmod 400 .globus/userkey.pem"
+install_cas
+
+setup_client_certificate
 
 execute "echo pass | voms-proxy-init --pwstdin --cert .globus/usercert.pem --key .globus/userkey.pem"
 
 # Remove emi-release package
 execute "yum -y remove emi-release"
 
-# Download EMI 3 repos & VOMS repos
-execute "wget -q $emi_repo -O $emi_repo_filename"
-
-if [ ! -z "$voms_repo" ]; then
-    execute "wget -q $voms_repo -O $voms_repo_filename"
-    execute "echo >> $voms_repo_filename; echo 'priority=1' >> $voms_repo_filename"
-fi
+install_emi_repo
+install_voms_repo
 
 # clean yum
 execute "yum clean all"
@@ -66,7 +47,6 @@ else
   execute "yum -y install voms-clients3"
   configure_vomsdir
 fi
-
 
 execute "echo pass | voms-proxy-init --pwstdin --cert .globus/usercert.pem --key .globus/userkey.pem"
 
