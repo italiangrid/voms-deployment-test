034481e6e3aeca843ebbe23d6f80a857c90eaf6f
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2014-05-14 11:51:53 +0200

    Update emi3-setup-sl5.sh

--- emi3-setup-sl5.sh
+++ emi3-setup-sl5.sh
@@ -4,7 +4,7 @@ export PLATFORM=SL5
 
 export DEFAULT_EMI2_RELEASE_PACKAGE=http://emisoft.web.cern.ch/emisoft/dist/EMI/2/sl5/x86_64/base/emi-release-2.0.0-1.sl5.noarch.rpm
 export DEFAULT_EMI_REPO=http://eticssoft.web.cern.ch/eticssoft/mock/emi-3-rc-sl5.repo
-export DEFAULT_VOMS_REPO=http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_voms_develop_SL5/lastSuccessfulBuild/artifact/voms-develop.repo
+export DEFAULT_VOMS_REPO=http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_voms_develop_SL5/lastSuccessfulBuild/artifact/voms-develop_sl5.repo
 export DEFAULT_TEST_CA_REPO=http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_test_ca/lastSuccessfulBuild/artifact/test-ca.repo
 
 # Do MySQL deployment tests by default
